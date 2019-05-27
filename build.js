abfa596d8927687b9b0636a9f11749ca472f89d6
unknown <mathieu@usp-3.fr>
2019-05-27 22:18:01 +0200

    useless file

--- build.js
+++ build.js
@@ -1,34 +1,11 @@
-var MSICreator = require('electron-wix-msi').MSICreator;
+var electronInstaller = require('electron-winstaller');
 
-// Step 1: Instantiate the MSICreator
-var msiCreator = new MSICreator({
-  appDirectory: 'C:\\Users\\Mathieu\\Documents\\GitHub\\instagram-pc',
-  description: 'Instagram for PC',
-  exe: 'instagram-pc',
-  name: 'Instagram PC',
-  manufacturer: 'Mathieu Colmon',
-  version: '1.0.0',
-  outputDirectory: 'C:\\Users\\Mathieu\\Documents\\GitHub\\instagram-pc\\build\\win-msi'
+
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: './build/instagram-pc-win32-x64',
+    outputDirectory: './build',
+    authors: 'Mathieu Colmon',
+    exe: 'instagram-pc.exe'
 });
 
-// Step 2: Create a .wxs template file
-msiCreator.create().then(function(value, error){
-    msiCreator.compile();
-
-});
-
-// Step 3: Compile the template to a .msi file
-
-
-
-// var electronInstaller = require('electron-winstaller');
-
-
-// resultPromise = electronInstaller.createWindowsInstaller({
-//     appDirectory: '/',
-//     outputDirectory: '/build/win',
-//     authors: 'Mathieu Colmon',
-//     exe: 'instagram-pc.exe'
-// });
-
-// resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+resultPromise.then(() => console.log("Built !"), (e) => console.log(`Error: ${e.message}`));
